db2462fefe032e76d1a3af8c13f9f7f27aea49fa
Francisco Santana <franciscosantana203@gmail.com>
2024-09-20 01:05:57 -0300

    chore(development):new branch

--- src/app/components/Card/CardNumber.tsx
+++ src/app/components/Card/CardNumber.tsx
@@ -4,5 +4,5 @@ type CardNumberProps = React.ComponentProps<'p'>;
 
 export const CardNumber = ({children}: CardNumberProps) => {
     return <p
-        className="absolute bottom-[3.8rem] left-[8%] text-3xl whitespace-nowrap block">{children}</p>
+        className="absolute  bottom-[3.8rem] left-[8%] text-3xl whitespace-nowrap block">{children}</p>
 }
